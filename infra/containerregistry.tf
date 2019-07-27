6331e98c275ac8d56e0496f651392f727ef4fa41
Wade G <wade.gausden@gmail.com>
2019-07-27 12:44:35 +1200

    No hypens allowed in container register

--- infra/containerregistry.tf
+++ infra/containerregistry.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "acr" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "rushtodo-containerregistry"
+  name                     = "rushtodocontainerregistry"
   resource_group_name      = "${azurerm_resource_group.acr.name}"
   location                 = "${azurerm_resource_group.acr.location}"
   sku                      = "Premium"
