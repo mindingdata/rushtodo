da27db6e5a025858781875d661ed10c37369232c
Wade G <wade.gausden@gmail.com>
2019-07-28 12:20:41 +1200

    Remove app setting

--- infra/appservice.tf
+++ infra/appservice.tf
@@ -20,10 +20,6 @@ resource "azurerm_app_service" "appservice" {
   resource_group_name = "${azurerm_resource_group.appservice.name}"
   app_service_plan_id = "${azurerm_app_service_plan.appservice.id}"
 
-  site_config {
-    "always_on" = "true"
-  }
-
   app_settings = {
     "ASPNETCORE_ENVIRONMENT" = "Production"
   }
